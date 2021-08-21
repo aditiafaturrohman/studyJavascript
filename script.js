ebd1d1786ffc76dd2c06dfd3ca55358a4a801f48
aditiafaturrohman <aditiafaturrohman011003@gmail.com>
2021-08-21 22:57:16 +0700

    study javascript DOM ke 10

--- script.js
+++ script.js
@@ -1,49 +1,21 @@
-const ubahWarna = document.getElementById("ubahWarna");
-ubahWarna.onclick = function () {
-  //   document.body.style.backgroundColor = "lightgreen";
-  //   document.body.setAttribute("class", "biru-muda");
-  document.body.classList.toggle("biru-muda");
-};
+// const close = document.querySelector(".close");
+// const card = document.querySelector(".card");
 
-const acakWarna = document.createElement("button");
-const teksAcakWarna = document.createTextNode("acakWarna");
-acakWarna.appendChild(teksAcakWarna);
-ubahWarna.after(acakWarna);
-acakWarna.setAttribute("type", "button");
+// close.addEventListener("click", function () {
+//   card.style.display = "none";
+// });
 
-acakWarna.addEventListener("click", function () {
-  const r = Math.round(Math.random() * 255 + 1);
-  const g = Math.round(Math.random() * 255 + 1);
-  const b = Math.round(Math.random() * 255 + 1);
+const close = document.querySelectorAll(".close");
 
-  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
-});
+// for (let i = 0; i < close.length; i++) {
+//   close[i].addEventListener("click", function (e) {
+//     // close[i].parentElement.style.display = "none";
+//     e.target.parentElement.style.display = "none";
+//   });
+// }
 
-const sMerah = document.querySelector("input[name = sMerah]");
-sMerah.addEventListener("input", function () {
-  const r = sMerah.value;
-  const g = sHijau.value;
-  const b = sBiru.value;
-  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
-});
-
-const sHijau = document.querySelector("input[name = sHijau]");
-sHijau.addEventListener("input", function () {
-  const r = sMerah.value;
-  const g = sHijau.value;
-  document.body.style.backgroundColor = "rgb(" + r + "," + g + ",100)";
-});
-
-const sBiru = document.querySelector("input[name = sBiru]");
-sBiru.addEventListener("input", function () {
-  const r = sMerah.value;
-  const g = sHijau.value;
-  const b = sBiru.value;
-  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
-});
-
-document.body.addEventListener("mousemove", function (event) {
-  const xPos = Math.round((event.clientX / window.innerWidth) * 255);
-  const yPos = Math.round((event.clientY / window.innerHeight) * 255);
-  document.body.style.backgroundColor = "rgb(" + xPos + "," + yPos + ",100)";
+close.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.target.parentElement.style.display = "none";
+  });
 });
