0d81e7f1c03340cee999b7a01858d08a7b01d8ac
aditiafaturrohman <aditiafaturrohman011003@gmail.com>
2021-08-29 00:22:20 +0700

    learn javascript DOM 28

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
+const mhs1 = {
+  nama: "aditia faturrohman",
+  umur: 17,
+  jurusan: "TKJ",
+  alamat: { desa: "ciakar", kecamatan: "panongan", kota: "tanggerang", provinsi: "banten" },
+};
 
-// mencari angka >= 3
-// const newAngka = [];
-// for (let i = 0; i <= angka.length; i++) {
-//   if (angka[i] >= 3) {
-//     newAngka.push(angka[i]);
-//   }
-// }
+function mhs({ nama, umur, jurusan, alamat: { desa, kecamatan, kota, provinsi } }) {
+  return `nama saya ${nama}, umur saya ${umur}, saya dari fakultas ${jurusan}, asal saya dari ${kota}`;
+}
 
-// console.info(newAngka);
-
-// const newAngka = angka.filter((a) => a >= 3);
-
-// const newAngka = angka.map((a) => a * 2);
-
-// const newAngka = angka.reduce((accumulator, currentValue) => accumulator + currentValue);
-
-const hasil = angka
-  .filter((a) => a > 5)
-  .map((a) => a * 3)
-  .reduce((acc, cur) => acc + cur);
-
-console.info(hasil);
+console.info(mhs(mhs1));
