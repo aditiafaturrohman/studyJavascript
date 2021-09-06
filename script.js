2bee3960be7f85f02d4421646e7fba5a4f5572d7
aditiafaturrohman <aditiafaturrohman011003@gmail.com>
2021-09-07 00:49:02 +0700

    learn javascript last chapter

--- script.js
+++ script.js
@@ -1,67 +1,33 @@
-$(".search-button").on("click", function () {
-  $.ajax({
-    url: "http://www.omdbapi.com/?apikey=f7078c20&s=" + $(".input-keyword").val(),
-    success: (results) => {
-      const movies = results.Search;
-      let cards = "";
-      movies.forEach((m) => {
-        cards += showCards(m);
-      });
-      $(".movie-container").html(cards);
+// const coba = new Promise((resolve) => {
+//   setTimeout(() => {
+//     alert("berhasil");
+//     resolve("oke");
+//   }, 2000);
+// });
 
-      // ketika tombol di klik
-      $(".modal-detail-button").on("click", function () {
-        //   console.log($(this).data("imdbid"));
-        $.ajax({
-          url: "http://www.omdbapi.com/?apikey=f7078c20&i=" + $(this).data("film"),
-          success: (m) => {
-            const movieDetail = showMovieDetail(m);
-            $(".modal-body").html(movieDetail);
-          },
-          error: (e) => {
-            console.info(e);
-          },
-        });
-      });
-    },
+// coba.then(() => console.info(coba));
 
-    error: (e) => {
-      console.info(e);
-    },
+function cobaPromise() {
+  return new Promise((resolve, rejected) => {
+    const waktu = 5000;
+    if (waktu <= 5000) {
+      setTimeout(() => {
+        const input = prompt("siapa nama anda");
+        alert(`hello ${input}`);
+      }, waktu);
+    } else {
+      console.error(err);
+    }
   });
-});
-
-function showCards(m) {
-  return `<div class="col-md-4 my-3">
-    <div class="card">
-        <img src="${m.Poster}" class="card-img-top" alt="" />
-            <div class="card-body">
-                <h5 class="card-title">${m.Title}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-film="${m.imdbID}">Show details</a>
-            </div>
-    </div>
-</div>`;
 }
 
-function showMovieDetail(m) {
-  return `<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-3">
-        <img src="${m.Poster}" class="img-fluid" />
-        </div>
-        <div class="col-md">
-        <ul class="list-group">
-            <li class="list-group-item"><h4>${m.Title} ${m.Year}</h4></li>
-            <li class="list-group-item"><strong>Director : </strong>${m.Director}</li>
-            <li class="list-group-item"><strong>Actors : </strong>${m.Actors}</li>
-            <li class="list-group-item"><strong>Writer : </strong>${m.Writer}</li>
-            <li class="list-group-item">
-            <strong>Plot : </strong> <br />
-            ${m.Plot}
-            </li>
-        </ul>
-        </div>
-      </div>
-    </div>`;
+async function cobaAsync() {
+  try {
+    const coba = await cobaPromise();
+    console.info(coba);
+  } catch (err) {
+    console.error("kelamaan");
+  }
 }
+
+cobaAsync();
